5c2805a3f55364b30483eaf92e7b533a88309e79
ibrakhimzhanov <unkiborg@gmail.com>
2020-10-02 18:29:07 +0500

    Залогировал об успешной регистрации клиента

--- bot.py
+++ bot.py
@@ -92,6 +92,7 @@ class Bot:
         else:
             # search intent
             for intent in settings.INTENTS:
+                log.debug(f'User gets {intent}')
                 if any(token in text for token in intent['tokens']):
                     if intent['answer']:
                         text_to_send = intent['answer']
@@ -130,6 +131,7 @@ class Bot:
                 state.step_name = step['next_step']
             else:
                 # finish scenario
+                log.info('Зарегистрирован: {name} {email}'.format(**state.context))
                 self.user_states.pop(user_id)
         else:
             # retry current step
